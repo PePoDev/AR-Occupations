4ed7329ad91a7a5649d6cd37cbaa43c8ff14f136
Thiwanon Chomcharoen <gmgame.1525@gmail.com>
2019-12-16 17:33:13 +0700

    Add Screenshot

--- Assets/Scripts/Playground.cs
+++ Assets/Scripts/Playground.cs
@@ -1,12 +1,18 @@
-﻿using ImgurAPI;
+﻿using System.IO;
+using ImgurAPI;
 using UnityEngine;
 
 public class Playground : MonoBehaviour
 {
     private void Start()
     {
+        var path = $"{Application.dataPath}/screenshots";
         var fileName = "123";
-        ScreenCapture.CaptureScreenshot($@"{Application.dataPath}/screenshots/{fileName}.png");
+        if (!Directory.Exists(path))
+        {
+            Directory.CreateDirectory(path);
+        }
+        ScreenCapture.CaptureScreenshot($@"{path}/{fileName}.png");
         // Imgur.Upload(@"C:\Users\PePoDev\Pictures\Wallpaper\apple-ipad-pro-2018-stock-4k-46-1920x1080.jpg");
     }
 }
