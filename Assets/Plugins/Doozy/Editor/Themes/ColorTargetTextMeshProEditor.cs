d23d7ad649939ebb51a7e54509636c3292f006ea
Thiwanon Chomcharoen <gmgame.1525@gmail.com>
2019-12-18 17:19:34 +0700

    Create Playground scene

--- Assets/Plugins/Doozy/Editor/Themes/ColorTargetTextMeshProEditor.cs
+++ Assets/Plugins/Doozy/Editor/Themes/ColorTargetTextMeshProEditor.cs
@@ -2,6 +2,7 @@
 // This code can only be used under the standard Unity Asset Store End User License Agreement
 // A Copy of the EULA APPENDIX 1 is available at http://unity3d.com/company/legal/as_terms
 
+using System;
 using Doozy.Editor.Internal;
 using Doozy.Engine.Themes;
 using Doozy.Engine.Utils;
